fef75b8109d25c1578d97bae437fef3933c254d0
Ben Parisot <me@benparisot.com>
2019-01-30 11:32:17 -0800

    set up project, connect index.html in root to main js file, write first test

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const pickForm = document.getElementById('pick-form');
+
+
+
+pickForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    const randomNumber = Math.random();
+
+});
+//grab form node form DOM
+
+//add event listener to form node
+
+//have button/form submit generate random number
+
+//create function that takes random number and either returns dog or cat
