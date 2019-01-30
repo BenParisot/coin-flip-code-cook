443e5ec6ddab85558f48ee2de8ebd0d00b053a6f
Ben Parisot <me@benparisot.com>
2019-01-30 14:27:35 -0800

    get catCount and dogCount to show on the html page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import catOrDog from './cat-or-dog.js';
 
 const pickForm = document.getElementById('pick-form');
 const image = document.getElementById('animal-pic');
+const catNode = document.getElementById('cat-count');
+const dogNode = document.getElementById('dog-count');
+
+let catCount = 0;
+let dogCount = 0;
+
 
 pickForm.addEventListener('submit', function(event){
     event.preventDefault();
@@ -9,15 +15,23 @@ pickForm.addEventListener('submit', function(event){
     const animal = catOrDog(randomNumber);
     let imageSource = '';
 
+
     if(animal === 'cat') {
         imageSource = '../assets/cat.jpg';
+        catCount++;
+        catNode.textContent = catCount;
+
     }
     else {
         imageSource = '../assets/dog.jpg';
+        dogCount++;
+        dogNode.textContent = dogCount;
     }
 
     image.src = imageSource;
     image.classList.remove('hidden');
+
+
 
 });
 //grab form node from DOM
