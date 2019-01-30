e05680c3e7660e128b1b176f919090c8dc52fbf9
Ben Parisot <me@benparisot.com>
2019-01-30 11:58:59 -0800

    finish test for function that returns ca or dog based on random number

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,15 @@
 const test = QUnit.test;
 
 function catOrDog(randomNumber) {
+    let result = '';
+
     if(randomNumber < 0.5) {
-        return 'cat';
+        result = 'cat';
     }
+    else {
+        result = 'dog';
+    }
+    return result;
 }
 
 
@@ -11,6 +17,14 @@ test('return cat if random number is less than 0.5', function(assert) {
     const randomNumber = 0.4;
     const result = catOrDog(randomNumber);
     const expected = 'cat';
+
+    assert.equal(result, expected);
+});
+
+test('return dog if random number is equal to or more than 0.5', function(assert) {
+    const randomNumber = 0.5;
+    const result = catOrDog(randomNumber);
+    const expected = 'dog';
 
     assert.equal(result, expected);
 });
